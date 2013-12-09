4e9eed79f211c929a9e1590489c4579503b74c0a
Jens Lindberg <elusiveunit@gmail.com>
2013-12-09 21:37:04 +0100

    __FILE__ symlink workaround, trac ticket #16953

--- lucid-slider.php
+++ lucid-slider.php
@@ -18,22 +18,29 @@
 // Block direct requests
 if ( ! defined( 'ABSPATH' ) ) die( 'Nope' );
 
+// Symlink workaround, see http://core.trac.wordpress.org/ticket/16953
+$lucid_slider_plugin_file = __FILE__;
+if ( isset( $plugin ) )
+	$lucid_slider_plugin_file = $plugin;
+elseif ( isset( $network_plugin ) )
+	$lucid_slider_plugin_file = $network_plugin;
+
 // Plugin constants
 if ( ! defined( 'LUCID_SLIDER_VERSION' ) )
 	define( 'LUCID_SLIDER_VERSION', '1.5.1' );
 
 if ( ! defined( 'LUCID_SLIDER_URL' ) )
-	define( 'LUCID_SLIDER_URL', trailingslashit( plugin_dir_url( __FILE__ ) ) );
+	define( 'LUCID_SLIDER_URL', trailingslashit( plugin_dir_url( $lucid_slider_plugin_file ) ) );
 
 if ( ! defined( 'LUCID_SLIDER_ASSETS' ) )
 	define( 'LUCID_SLIDER_ASSETS', LUCID_SLIDER_URL . 'assets/' );
 
 if ( ! defined( 'LUCID_SLIDER_PATH' ) )
-	define( 'LUCID_SLIDER_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
+	define( 'LUCID_SLIDER_PATH', trailingslashit( plugin_dir_path( $lucid_slider_plugin_file ) ) );
 
 // Misc. utility functions
 require LUCID_SLIDER_PATH . 'inc/utility.php';
 
 // Load and initialize the plugin parts
 require LUCID_SLIDER_PATH . 'inc/core.php';
-$lucid_slider_core = new Lucid_Slider_Core( __FILE__ );
+$lucid_slider_core = new Lucid_Slider_Core( $lucid_slider_plugin_file );
