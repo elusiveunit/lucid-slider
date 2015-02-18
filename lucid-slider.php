ef5ff3ba1187c970717ffdfd7a08d1e5d5583eb8
Jens Lindberg <elusiveunit@gmail.com>
2015-02-18 21:18:23 +0100

    Remove the __FILE__ symlink workaround
    The fix landed in WordPress 3.9

--- lucid-slider.php
+++ lucid-slider.php
@@ -17,31 +17,22 @@
 // Block direct requests
 if ( ! defined( 'ABSPATH' ) ) die( 'Nope' );
 
-// Symlink workaround, see http://core.trac.wordpress.org/ticket/16953
-// The root check is to stop a fatal error on activation
-$lucid_slider_plugin_file = __FILE__;
-$lucid_slider_document_root = str_replace( array( '\\', '/' ), DIRECTORY_SEPARATOR, $_SERVER['DOCUMENT_ROOT'] );
-if ( isset( $plugin ) && false !== strpos( $plugin, $lucid_slider_document_root ) )
-	$lucid_slider_plugin_file = $plugin;
-elseif ( isset( $network_plugin ) && false !== strpos( $network_plugin, $lucid_slider_document_root ) )
-	$lucid_slider_plugin_file = $network_plugin;
-
 // Plugin constants
 if ( ! defined( 'LUCID_SLIDER_VERSION' ) )
 	define( 'LUCID_SLIDER_VERSION', '1.7.0' );
 
 if ( ! defined( 'LUCID_SLIDER_URL' ) )
-	define( 'LUCID_SLIDER_URL', trailingslashit( plugin_dir_url( $lucid_slider_plugin_file ) ) );
+	define( 'LUCID_SLIDER_URL', trailingslashit( plugin_dir_url( __FILE__ ) ) );
 
 if ( ! defined( 'LUCID_SLIDER_ASSETS' ) )
 	define( 'LUCID_SLIDER_ASSETS', LUCID_SLIDER_URL . 'assets/' );
 
 if ( ! defined( 'LUCID_SLIDER_PATH' ) )
-	define( 'LUCID_SLIDER_PATH', trailingslashit( plugin_dir_path( $lucid_slider_plugin_file ) ) );
+	define( 'LUCID_SLIDER_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
 
 // Misc. utility functions
 require LUCID_SLIDER_PATH . 'inc/utility.php';
 
 // Load and initialize the plugin parts
 require LUCID_SLIDER_PATH . 'inc/core.php';
-$lucid_slider_core = new Lucid_Slider_Core( $lucid_slider_plugin_file );
+$lucid_slider_core = new Lucid_Slider_Core( __FILE__ );
